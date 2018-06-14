658d9c12e07d6c9962eb1323407333e5bc1c73ad
unknown <jim88star@gmail.com>
2018-06-14 02:58:41 -0500

    updated all files

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+var friends = [
+
+  {
+    name: "Billy",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Billy_IDOL_2012.JPG/1200px-Billy_IDOL_2012.JPG",
+    scores:[
+      "5",
+      "1",
+      "2",
+      "4",
+      "5",
+      "1",
+      "3",
+      "3",
+      "1",
+      "2"
+    ]
+  },
+
+  {
+    name: "Mick",
+    photo: "http://ultimateclassicrock.com/files/2017/03/MickJagger.jpg",
+    scores:[
+      "1",
+      "3",
+      "5",
+      "3",
+      "1",
+      "2",
+      "4",
+      "5",
+      "2",
+      "5"
+    ]
+  },
+
+  {
+    name: "Madonna",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Madonna_Rebel_Heart_Tour_2015_-_Stockholm_%2823051472299%29_%28cropped_2%29.jpg/1200px-Madonna_Rebel_Heart_Tour_2015_-_Stockholm_%2823051472299%29_%28cropped_2%29.jpg",
+    scores:[
+      "4",
+      "5",
+      "3",
+      "2",
+      "2",
+      "4",
+      "5",
+      "5",
+      "4",
+      "3"
+    ]
+  },
+
+  {
+    name: "Cyndi",
+    photo: "http://inspirer.life/home/wp-content/uploads/2017/01/original.jpg",
+    scores:[
+      "4",
+      "3",
+      "2",
+      "4",
+      "1",
+      "1",
+      "2",
+      "5",
+      "1",
+      "5"
+    ]
+  },
+
+  {
+    name: "Marilyn",
+    photo: "https://img.wennermedia.com/featured-promo-724/marilyn-manson-67cc120c-5a35-4fb6-b8a3-1c40ed37ccf4.jpg",
+    scores:[
+      "4",
+      "2",
+      "5",
+      "4",
+      "1",
+      "1",
+      "2",
+      "2",
+      "1",
+      "1"
+    ]
+  }
+];
+
+module.exports = friends;
